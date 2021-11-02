1cf62cff07902066a241b63f5ea8944bbf5927ce
Kenneth Mak <makk4@mcmaster.ca>
2021-11-02 13:14:48 -0400

    Added map to results

---
+++ assets/js/results.js
@@ -0,0 +1,62 @@
+var mymap;
+function initialize() {
+    console.log("Loading map");
+    mymap = L.map('mymap');
+    // use the OpenStreetMaps tiles
+    initializeMap();
+
+
+    // sets location/center of map
+    mymap.setView([43.263,-79.919], 10);
+    addMarkerNL( 43.263,-79.919, "Your location");
+
+    // Hamilton Archery Centre
+    addMarker( 43.240260, -79.789990,
+        "Hamilton Archery Centre", "object.html");
+        
+    // Evolve Archery Canada
+    addMarker( 43.495470, -79.886850,
+        "Evolve Archery Canada", "object.html");
+        
+    // Silver Sword Armories
+    addMarker( 43.497740, -79.742210,
+        "Silver Sword Armories", "object.html");
+        
+    // Badenoch Archery
+    addMarker( 43.449300, -80.116240,
+        "Badenoch Archery", "object.html");
+
+        // create a blank pop-up for later use
+    // display a popup when the user clicks on the map
+    function onMapClick(e) {
+        var popup = L.popup();
+        popup.setLatLng(e.latlng)
+        //popup.setContent("You clicked the map at " + e.latlng.toString())
+        //popup.openOn(mymap);
+        console.log(e.latlng);
+    }
+    mymap.on('click', onMapClick);
+}
+
+function initializeMap() {
+    L.tileLayer(
+        'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        { attribution: 'Map data &copy; <a href="http://openstreetmap.org"> OpenStreetMap</a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>' }
+        ).addTo(mymap);
+}
+
+// creates a marker with the latitude, longitude, location name, and the link to the location
+// atm we just link to the sample object
+function addMarker(lat, long, locName, locLink) {
+    L.marker([lat, long])
+    .addTo(mymap)
+    .bindPopup("<b>" + locName + "</b>" +       // name in bold
+                "<br>" +                            // newline
+                "<a href='" + locLink + "'>Link</a>")   // link
+}
+// marker with no link
+function addMarkerNL(lat, long, locName) {
+    L.marker([lat, long])
+    .addTo(mymap)
+    .bindPopup("<b>" + locName + "</b>")      // name in bold
+}
